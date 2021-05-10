f05ba4b7a4d8a5de608001aaf31e6e4620b1ade7
Dixit-Mendon <dixitmendon14@gmail.com>
2021-05-10 12:27:39 +0530

    Added all the functionalities

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -2,14 +2,22 @@
 import React, { useState, useEffect } from 'react';
 import queryString from 'query-string';
 import io from 'socket.io-client';
+import './Chat.css';
+
+import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
+import Messages from '../Messages/Messages';
+import TextContainer from '../TextContainer/TextContainer';
 
 let socket;
-
 
 const Chat = ({ location }) => {
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
-    const ENDPOINT = 'localhost:5000';
+    const [users, setUsers] = useState('');
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
+    const ENDPOINT = 'http://localhost:5000';
 
     useEffect(() => {
         const { name, room } = queryString.parse(location.search);
@@ -26,15 +34,45 @@ const Chat = ({ location }) => {
         setName(name);
         setRoom(room);
 
-        socket.emit('join', { name, room }, ({ error }) => {
-            alert(error);
+        socket.emit('join', { name, room }, () => {
+
         });
 
-        console.log(socket);
+        return () => {
+            socket.emit('disconnect');
+            socket.off();
+        }
     }, [ENDPOINT, location.search]);
 
+    useEffect(() => {
+        socket.on('message', (message) => {
+            setMessages([...messages, message]);
+        });
+
+        socket.on("roomData", ({ users }) => {
+            setUsers(users);
+        });
+    }, [messages]);
+
+    //function for sending messages
+    const sendMessage = (event) => {
+        event.preventDefault();
+
+        if (message) {
+            socket.emit('sendMessage', message, () => setMessage(''));
+        }
+    }
+    console.log(users);
+
     return (
-        <h1>Chat</h1>
+        <div className="outerContainer">
+            <div className="container">
+                <InfoBar room={room} />
+                <Messages messages={messages} name={name} />
+                <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
+            </div>
+            <TextContainer users={users} />
+        </div>
     );
 }
 
